164425098756d9bda14df0215faa8c705d827d96
James Wilson <jgwilson1214@gmail.com>
2019-01-16 17:42:50 -0800

    use functions for 1,2

--- basics.go
+++ basics.go
@@ -13,30 +13,50 @@ func main() {
 	/**************************************/
 	print("Convert hex to base64\n")
 	const s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	decoded, err := hex.DecodeString(s)
-	if err != nil {
-		log.Fatal(err)
-	}
-	sol_1 := base64.StdEncoding.EncodeToString(decoded)
-	fmt.Printf("%v\n", sol_1)
+	fmt.Printf("%v\n", hex2base64(s))
 	/**************************************/
 	print("Fixed XOR\n")
 	const s2 = "1c0111001f010100061a024b53535009181c"
-	decoded_s2, err := hex.DecodeString(s2)
-	if err != nil {
-		log.Fatal(err)
-	}
 	const xor = "686974207468652062756c6c277320657965"
-	decoded_xor, err := hex.DecodeString(xor)
+	fmt.Printf("%x\n", fixedXOR(s2, xor))
+	/**************************************/
+	print("Single-byte XOR cipher\n")
+	const s3 = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+}
+
+/* Converts a hex string to base64
+ * and returns string result. */
+func hex2base64(s string) string {
+	var raw []byte
+
+	raw, err := hex.DecodeString(s)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var fixed_xor []byte
-	for i, _ := range decoded_s2 {
-		fixed_xor = append(fixed_xor, decoded_s2[i]^decoded_xor[i])
-	}
-	sol_2 := hex.EncodeToString(fixed_xor)
-	fmt.Printf("%s\n", sol_2)
-	/**************************************/
 
+	return base64.StdEncoding.EncodeToString(raw)
+
+}
+
+/* XOR's two equal-length hex strings
+ * and returns the byte slice result */
+func fixedXOR(s1, s2 string) []byte {
+	var xor []byte
+	var raw_1 []byte
+	var raw_2 []byte
+
+	raw_1, err := hex.DecodeString(s1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	raw_2, err = hex.DecodeString(s2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, _ := range raw_1 {
+		xor = append(xor, raw_1[i]^raw_2[i])
+	}
+
+	return xor
 }
